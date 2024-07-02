7dd6dff8db5baf0df57b644a8ab4a0465d560d56
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-02 09:24:59 +0100

    first

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -28,19 +28,20 @@ def get_weather_data(lat, lon):
     weather_url = requests.get(f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_api_key}&units=metric")
     weather_data = weather_url.json()
     main_weather = weather_data.get('main', {})
-    temp = main_weather.get('temp', 273.15)
-    return temp - 273.15
+    temp = main_weather.get('temp', 0)
+    return temp
 
 
 def greeting(request):
     visitor_name = request.GET.get('visitor_name', 'guest')
     ip_address = get_client_ip(request)
     city, lat, lon = get_location_data(ip_address)
-    temp_celsius = get_weather_data(lat, lon)
+
+    temp = get_weather_data(lat, lon)
 
     return JsonResponse({
         "client_ip": ip_address,
         "location": city,
-        "greeting": f"Hello, {visitor_name}!, the temperature is {temp_celsius:.2f} degrees Celsius in {city}"
+        "greeting": f"Hello, {visitor_name}!, the temperature is {temp:.2f} degrees Celsius in {city}"
     })
 
